2bd7d0c4736de9eb7172e69adc77c06067411033
adewale <drtoluoni2017@gmail.com>
2021-02-04 01:08:41 +0100

    more on stack

--- stack/stack.py
+++ stack/stack.py
@@ -26,3 +26,48 @@ print(x.isEmpty())
 print(x.size())
 print(x.pop())
 # print(x)
+
+
+class myStack:
+    def __init__(self):
+        self.arr = []
+        self.length = 0
+
+    def __str__(self):
+        return str(self.__dict__)
+
+    def peek(self):
+        return self.arr[self.length - 1]
+
+    def push(self, value):
+        self.arr.append(value)
+        self.length += 1
+
+    def pop(self):
+        popped_item = self.arr[self.length - 1]
+        del self.arr[self.length - 1]
+        self.length -= 1
+        return popped_item
+
+
+
+estack = myStack()
+estack.push('chickenRepublic')
+estack.push('kfc')
+estack.push('mybig')
+
+print(estack)
+
+estack.push('bestmeal')
+
+print(estack)
+
+estack.pop()
+
+print(estack)
+
+x = estack.peek()
+
+print(x)
+
+print(estack)
